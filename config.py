5f50f7691569f1f86c792deb8206d3be5429dc3b
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-16 11:05:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,14 @@ UNIVERSES = {
 }
 
 # Rolling windows to evaluate (days)
-WINDOWS = [126, 252]
+WINDOWS = [126, 252]          # two windows for speed
 
-# Signature depth 
-SIG_DEPTH = 2
+# Signature depth (2 = 6 features, 3 = 14 features)
+SIG_DEPTH = 2                 # use 2 for faster training
 
-# SVM hyperparameters 
-SVM_KERNEL = "linear"
-
+# SVM hyperparameters
+SVM_C = 1.0
+SVM_KERNEL = "linear"         # linear is faster than rbf
+SVM_GAMMA = "scale"           # only used if kernel = 'rbf'
 
 TOP_N = 3
